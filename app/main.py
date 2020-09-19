744328e3363bc3f616cdef196e47abfd550db8ce
Vaishnavi Deshpande <vaishdesh2001@gmail.com>
2020-09-19 09:17:59 -0500

    now points to index.html

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, flash
 app = Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 @app.route("/")
 def home_view():
-	return "<h1>CourseCash!!!</h1>"
+	return render_template('index.html')
